be5f04ac5885217011d95fae5a3bfc4e4338f9d1
thanhtrung1999 <darkprince411999@gmail.com>
2021-09-22 14:29:38 +0700

    add user's route

--- routes/user.php
+++ routes/user.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\HomeController;
+use App\Http\Controllers\User\ProductController;
+use App\Http\Controllers\User\CartController;
+use App\Http\Controllers\User\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('', function () {
+   return redirect()->route('home');
+});
+
+Route::get('trang-chu', [HomeController::class, 'index'])->name('home');
+Route::prefix('san-pham')->group(function () {
+    Route::get('', [ProductController::class, 'index'])->name('products.index');
+    Route::get('chi-tiet', [ProductController::class, 'detail'])->name('products.detail');
+});
+Route::get('gio-hang', [CartController::class, 'index'])->name('cart.index');
+Route::get('thanh-toan', [PaymentController::class, 'index'])->name('payment.index');
