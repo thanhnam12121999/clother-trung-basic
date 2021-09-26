c5fd6945b58b78cbb4d73afea2690c1e4308b3d7
namnt <namnt@deha-soft.com>
2021-09-26 20:53:06 +0700

    [fake_data] - Fake data for all tables .

--- app/Models/ProductVariantImage.php
+++ app/Models/ProductVariantImage.php
@@ -11,12 +11,12 @@ class ProductVariantImage extends Model
     use HasFactory;
     use SoftDeletes;
 
-    protected $table = "product_variant_images";
+    protected $table = "product_images";
     protected $fillable = [
         'product_variant_id',
         'image'
     ];
-
+    
     public function variants()
     {
         return $this->belongsTo(ProductVariant::class, 'product_variant_id', 'id');
