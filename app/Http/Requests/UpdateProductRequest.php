7520751cecc04ad078aef1b4f311fc2e5e604082
namnt <namnt@deha-soft.com>
2021-09-30 22:48:05 +0700

    Create Update and Store Product .

---
+++ app/Http/Requests/UpdateProductRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:5|max:200',
+            'cate_id' => 'required|exists:categories,id',
+            'feature_image' => 'image|mimes:jpeg,png,jpg,svg|max:2048',
+            'image_list' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'status' => 'required|boolean',
+        ];
+    }
+}
