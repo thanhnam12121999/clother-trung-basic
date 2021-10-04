1349a7b002aa578cf0ca7037fccd35f754fb3b2b
darkprince411999 <darkprince411999@gmail.com>
2021-10-05 02:04:31 +0700

    CRUD categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -14,10 +15,22 @@ class Category extends Model
     protected $table = "categories";
     protected $fillable = [
         'name',
+        'slug',
         'image',
         'description',
         'parent'
     ];
+
+    protected static function booted()
+    {
+        parent::booted();
+        static::creating(function ($category) {
+            $category->slug = Str::slug($category->name);
+        });
+        static::updating(function ($category) {
+            $category->slug = Str::slug($category->name);
+        });
+    }
 
     public function products()
     {
--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -3,17 +3,86 @@
 namespace App\Services;
 
 use App\Repositories\CategoryRepository;
+use App\Traits\HandleImage;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class CategoryService extends BaseService
 {
-    protected $categoryRepository;
+    use HandleImage;
 
-    public function __construct(CategoryRepository $categoryRepository)
+    protected $categoryRepository;
+    protected $productService;
+
+    public function __construct(CategoryRepository $categoryRepository, ProductService $productService)
     {
         $this->categoryRepository = $categoryRepository;
+        $this->productService = $productService;
     }
-    
-    public function getAll(){
+
+    public function getAll()
+    {
         return $this->categoryRepository->all();
+    }
+
+    public function handle(Request $request, $category = null)
+    {
+        try {
+            $categoryData = $request->only(['name', 'description']);
+            DB::beginTransaction();
+            if ($request->hasFile('image')) {
+                if (!empty($category) && !empty($category->image)) {
+                    $this->deleteImage($category, 'categories');
+                }
+                $imageName = $this->createImage($request->file('image'), 'categories');
+                if (!$imageName) {
+                    return $this->sendError('Lỗi thêm ảnh danh mục, vui lòng thử lại', Response::HTTP_BAD_REQUEST);
+                }
+                $categoryData['image'] = $imageName;
+            }
+            $responseMsg = '';
+            if (empty($category)) {
+                $responseMsg = 'Thêm danh mục thành công';
+                $this->categoryRepository->create($categoryData);
+            } else {
+                $responseMsg = 'Chỉnh sửa danh mục thành công';
+                $this->categoryRepository->update($category->id, $categoryData);
+            }
+            DB::commit();
+            return $this->sendResponse($responseMsg);
+        } catch (\Exception $e) {
+            Log::error($e);
+            DB::rollBack();
+        }
+        return $this->sendError('Có lỗi xảy ra, vui lòng thử lại');
+    }
+
+    public function delete($category)
+    {
+        try {
+            DB::beginTransaction();
+            $isDelete = $this->deleteImage($category->image, 'categories');
+            if (!$isDelete) {
+                throw new \Exception;
+            }
+            $products = $category->products;
+            if ($products->isNotEmpty()) {
+                $isDeleteProductsData = $this->productService->deleteMultipleProducts($products);
+                if (!$isDeleteProductsData) {
+                    throw new \Exception;
+                }
+            }
+            $category->delete();
+            DB::commit();
+            return $this->sendResponse('Xóa danh mục thành công');
+        } catch (\Exception $e) {
+            Log::error($e);
+            DB::rollBack();
+        }
+        return $this->sendError('Xóa danh mục thất bại');
     }
 }
