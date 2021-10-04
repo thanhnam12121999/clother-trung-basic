044f476d2a8faab1246face7a72f433cd32d988c
darkprince411999 <darkprince411999@gmail.com>
2021-10-05 02:09:48 +0700

    modify route admin

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Admin\AuthController;
-use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ManagerController;
 use App\Http\Controllers\Admin\MemberController;
@@ -20,28 +19,27 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/login', [AuthController::class, 'getViewLogin'])->name('login-page');
-
 
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 Route::get('order', [OrderController::class, 'index'])->name('orders.index');
 Route::get('manager', [ManagerController::class, 'index'])->name('manager.index');
 Route::get('slide', [SlideController::class, 'index'])->name('sldie.index');
 Route::get('member', [MemberController::class, 'index'])->name('member.index');
-// route category
-Route::prefix('category')->group(function() {
-    Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
-    Route::get('/create', [CategoryController::class, 'index'])->name('categories.create');
-});
-// 
+
+/**
+ * Route category
+ */
+Route::resource('categories', CategoryController::class)->except(['show']);
 
 Route::prefix('product')->group(function() {
-    Route::get('/delete/{id}', [ProductController::class, 'destroy'])->name('products.delete');
     Route::get('/', [ProductController::class, 'index'])->name('products.index');
-    Route::get('/create', [ProductController::class, 'getFormCreate'])->name('products.form_create');
-    Route::get('/view/{id}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/create', [ProductController::class, 'create'])->name('products.form_create');
     Route::post('/create', [ProductController::class, 'store'])->name('products.create');
-    Route::get('/edit/{id}', [ProductController::class, 'getFormEdit'])->name('products.edit');
+    Route::get('/view/{id}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
     Route::put('/update/{id}', [ProductController::class, 'update'])->name('products.update');
+    Route::get('/delete/{id}', [ProductController::class, 'destroy'])->name('products.delete');
+
+    Route::resource('attributes', AttributeController::class)->only(['store', 'update', 'destroy']);
 });
